ab1259e209b82e5c23a11f1f6185dae3559669cc
sangeethkc <keyceesangi@gmail.com>
2022-09-22 18:16:29 +0530

    show cart

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  console.log(isLoggedIn)
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const cartItems = useSelector((state) => state.cart.itemsList);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
